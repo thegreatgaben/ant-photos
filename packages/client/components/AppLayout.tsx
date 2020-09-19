d347a4bc40eb93af98b2ba96a7682db7dc07a1a5
Gabriel Tan <gabrieltan19@gmail.com>
2020-09-19 23:30:48 +0800

    mobile friendly sider

--- packages/client/components/AppLayout.tsx
+++ packages/client/components/AppLayout.tsx
@@ -1,4 +1,5 @@
 import {Layout, Menu} from 'antd';
+import { useState } from 'react';
 import {FileImageOutlined, FolderOpenOutlined} from '@ant-design/icons';
 
 import style from './AppLayout.module.scss';
@@ -8,37 +9,44 @@ const { Content, Sider } = Layout;
 
 export default function AppLayout({ children }) {
     const router = useRouter();
+    const [showMask, setShowMask] = useState(false);
 
     let selectedMenuItem = ['1'];
     if (router.pathname.includes('/albums')) selectedMenuItem = ['2'];
 
     return (
         <Layout className={style.layout}>
-            <Layout>
-                <Sider className={style.sider} width={200}>
-                    <div className={style.logoContainer}>
-                        <img alt="Logo" src="/images/logo.svg"/>
-                        <h5>Ant Photos</h5>
-                    </div>
-                    <Menu
-                        className={style.menu}
-                        theme="dark"
-                        mode="inline"
-                        selectedKeys={selectedMenuItem}
-                    >
-                        <Menu.Item key="1" icon={<FileImageOutlined/>} onClick={() => router.push('/')}>
-                            Photos
-                        </Menu.Item>
-                        <Menu.Item key="2" icon={<FolderOpenOutlined/>} onClick={() => router.push('/albums')}>
-                            Albums
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-                <Layout className={style.contentLayout}>
-                    <Content className={style.content}>
-                        {children}
-                    </Content>
-                </Layout>
+            { showMask &&
+                <div className={style.drawerMask}></div>
+            }
+            <Sider 
+                breakpoint="lg"
+                collapsedWidth="0"
+                className={style.sider} 
+                onCollapse={collapsed => setShowMask(!collapsed)}
+            >
+                <div className={style.logoContainer}>
+                    <img alt="Logo" src="/images/logo.svg"/>
+                    <h5>Ant Photos</h5>
+                </div>
+                <Menu
+                    className={style.menu}
+                    theme="dark"
+                    mode="inline"
+                    selectedKeys={selectedMenuItem}
+                >
+                    <Menu.Item key="1" icon={<FileImageOutlined/>} onClick={() => router.push('/')}>
+                        Photos
+                    </Menu.Item>
+                    <Menu.Item key="2" icon={<FolderOpenOutlined/>} onClick={() => router.push('/albums')}>
+                        Albums
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout className={style.contentLayout}>
+                <Content className={style.content}>
+                    {children}
+                </Content>
             </Layout>
         </Layout>
     );
